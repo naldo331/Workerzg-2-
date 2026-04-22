60bbffda81de6fe3711e5355563f1a3365ba1fe3
naldo331 <naldoclean234@gmail.com>
2026-04-22 11:59:48 -0500

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/ai-powered-job-category-suggestion.ts';
-import '@/ai/flows/ai-worker-recommendation.ts';
--- src/firebase/config.ts
+++
@@ -1,2 +0,0 @@
-import firebaseConfig from '../../firebase-applet-config.json';
-export { firebaseConfig };
